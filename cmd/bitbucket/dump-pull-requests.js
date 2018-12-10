bc41958f5fc9f953fb92449af6e0eefe8d43be9e
yclian <foss@yclian.com>
2018-12-10 21:10:32 +0800

    Switch to ES8 with Babel. No actual functionality changes.

--- cmd/bitbucket/dump-pull-requests.js
+++ cmd/bitbucket/dump-pull-requests.js
@@ -2,46 +2,27 @@
   "use strict"
 }())
 
-const rp = require("request-promise-native")
+import BitbucketHelper from "../../lib/bitbucket/helper"
 
 exports.command = "dump-pull-request <repository> <pull-request> [options]"
 exports.desc = "Dump everything about the specified pull request"
 exports.builder = (yargs) => {
   return yargs
     .env("BITBUCKET")
+    .option("output", {
+      alias: "o", 
+      choices: ["", ""]
+    })
     .demandOption(["url", "username", "password"])
 }
 exports.handler = (argv) => {
 
-  let [project, repository] = argv.repository.split("/")
-  let url = `${argv.url}/rest/api/1.0/projects/${project}/repos/${repository}/pull-requests/${argv.pullRequest}`
-
-  let request = {
-    uri: url,
-    auth: {
+  let bb = new BitbucketHelper(
+    argv.url, 
+    {
       username: argv.username,
       password: argv.password
-    },
-    json: true
-  }
+  })
 
-  rp(request)
-    .then((body) => {
-
-      let out = { pullRequest: body, activities: null }
-
-      rp({...request, ...{ uri: `${url}/activities`}})
-        .then((body) => {
-          out.activities = body
-          console.log(JSON.stringify(out, null, 2))
-        })
-        .catch((err) => {
-          console.log(`ERR: ${err}`)
-          process.exit(1)
-        })
-    })
-    .catch((err) => {
-        console.log(`ERR: ${err}`)
-        process.exit(1)
-    })
+  bb.getPullRequest(argv.repository, argv.pullRequest)
 }
